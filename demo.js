57f2d60a42ef99d8e36a453b4eb8c2eaa60b0166
zeredogo <zeredogo@gmail.com>
2024-09-11 02:04:12 +0100

    updating js file

--- demo.js
+++ demo.js
@@ -21,7 +21,7 @@ function origin(( area === residentalArea ), ( region === Country )) {
 const plot = 759;
 const seize = 849;
 
-function sumTotal(p = plot, s = seiz) {
+function sumTotal(p = plot, s = seize) {
  c
 };
 
